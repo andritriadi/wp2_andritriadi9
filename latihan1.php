281703a22898127043f393680ac42154cf2bde1c
andritriadi <90779422+andritriadi@users.noreply.github.com>
2022-01-07 23:09:55 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,20 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. Selamat belajar Web Programming";
+        //$this->load->view('view-Latihan1');
+    }
+    public function penjumlahan($nil1, $nil2)
+    {
+        $this->load->model('model_latihan1');
+
+        $data['nilai1'] = $nil1;
+        $data['nilai2'] = $nil2;
+        $data['hasil'] = $this->model_latihan1->jumlah($nil1, $nil2);
+        $hasil = $this->model_latihan1->jumlah($nil1, $nil2);
+
+        $this->load->view('view-Latihan', $data);
+    }
+}
